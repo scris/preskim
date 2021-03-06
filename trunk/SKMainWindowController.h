754bf932aee8f3379ecd20b621c538a8e3a82e7a
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2021-03-06 20:08:56 +0000

    Don't update find highlights when new find results arrive, to avoid repreating the animation

--- trunk/SKMainWindowController.h
+++ trunk/SKMainWindowController.h
@@ -182,6 +182,7 @@ enum {
         unsigned int updatingThumbnailSelection:1;
         unsigned int isAnimating:1;
         unsigned int updatingNoteSelection:1;
+        unsigned int updatingFindResults:1;
         unsigned int updatingColor:1;
         unsigned int updatingFont:1;
         unsigned int updatingFontAttributes:1;
