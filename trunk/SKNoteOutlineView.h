aee854c19f52c87df51581a8cc443a2b545aee9f
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2022-01-22 14:34:25 +0000

    Renamevariable and macro. Get outlineTableColumn instead of a column by id.

--- trunk/SKNoteOutlineView.h
+++ trunk/SKNoteOutlineView.h
@@ -48,8 +48,7 @@
 - (id <SKNoteOutlineViewDelegate>)delegate;
 - (void)setDelegate:(id <SKNoteOutlineViewDelegate>)newDelegate;
 
-@property (nonatomic, readonly) NSTableColumn *firstVisibleTableColumn;
-@property (nonatomic, readonly) NSTableColumn *lastVisibleTableColumn;
+@property (nonatomic, readonly) BOOL outlineColumnIsFirst;
 @property (nonatomic, readonly) CGFloat visibleColumnsWidth;
 
 @end
