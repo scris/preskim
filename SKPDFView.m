7979aaa133576891a96edca3275d7ef7142388e0
Christiaan Hofman <1167268+hofman@users.noreply.github.com>
2022-11-03 15:30:29 +0000

    pass whether to select in options, also allow to select while also showing the reading bar

--- SKPDFView.m
+++ SKPDFView.m
@@ -3030,7 +3030,7 @@ static inline CGFloat secondaryOutset(CGFloat x) {
 #pragma mark Sync
 
 // @@ Horizontal layout
-- (void)displayLineAtPoint:(NSPoint)point inPageAtIndex:(NSUInteger)pageIndex showReadingBar:(BOOL)showBar noSelect:(BOOL)noSelect {
+- (void)displayLineAtPoint:(NSPoint)point inPageAtIndex:(NSUInteger)pageIndex select:(BOOL)select showReadingBar:(BOOL)showBar {
     if (pageIndex < [[self document] pageCount]) {
         PDFPage *page = [[self document] pageAtIndex:pageIndex];
         PDFSelection *sel = [page selectionForLineAtPoint:point];
@@ -3064,7 +3064,8 @@ static inline CGFloat secondaryOutset(CGFloat x) {
                 } else {
                     [readingBar goToLine:line onPage:page];
                 }
-            } else if (noSelect == NO && [sel hasCharacters] && [self toolMode] == SKTextToolMode) {
+            }
+            if (select && [sel hasCharacters] && [self toolMode] == SKTextToolMode) {
                 [self setCurrentSelection:sel];
             }
         }
