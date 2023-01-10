f84d04d336e6ecb5a4644a6d10d7d9ec5ef3d415
Christiaan Hofman <1167268+hofman@users.noreply.github.com>
2023-01-10 10:40:36 +0000

    import Foundation

--- SkimTransitions/SKTPlugInLoader.h
+++ SkimTransitions/SKTPlugInLoader.h
@@ -6,6 +6,7 @@
 //  Copyright © 2019-2023 Skim. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
 #import <CoreImage/CoreImage.h>
 
 @interface SKTPlugInLoader : NSObject <CIPlugInRegistration>
