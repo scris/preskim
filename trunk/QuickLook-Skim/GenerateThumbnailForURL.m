c290c83c08fec434b7d2c1627c1ae3e3a3df581c
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2023-03-02 16:49:14 +0000

    don't replace deprecated enum values in quicklook generator

--- trunk/QuickLook-Skim/GenerateThumbnailForURL.m
+++ trunk/QuickLook-Skim/GenerateThumbnailForURL.m
@@ -51,13 +51,13 @@ static void drawBackgroundAndApplicationIconInCurrentContext(QLThumbnailRequestR
 {
     [[NSColor whiteColor] setFill];
     NSRect pageRect = { NSZeroPoint, _paperSize };
-    NSRectFillUsingOperation(pageRect, NSCompositingOperationSourceOver);
+    NSRectFillUsingOperation(pageRect, NSCompositeSourceOver);
     
     NSURL *iconURL = (NSURL *)CFBundleCopyResourceURL(QLThumbnailRequestGetGeneratorBundle(thumbnail), CFSTR("Skim"), CFSTR("icns"), NULL);
     NSImage *appIcon = [[NSImage alloc] initWithContentsOfFile:[iconURL path]];
     [iconURL release];
     
-    [appIcon drawInRect:_iconRect fromRect:NSZeroRect operation:NSCompositingOperationSourceOver fraction:0.3];
+    [appIcon drawInRect:_iconRect fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:0.3];
     [appIcon release];    
 }
 
