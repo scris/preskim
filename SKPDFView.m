1918ccb9fbbea360b1fe36cd1075578e3bac3781
Christiaan Hofman <1167268+hofman@users.noreply.github.com>
2023-07-31 14:34:37 +0000

    hide loupe when losing key window status

--- SKPDFView.m
+++ SKPDFView.m
@@ -3310,12 +3310,16 @@ static inline CGFloat secondaryOutset(CGFloat x) {
     }
     if ([[notification name] isEqualToString:NSWindowDidResignKeyNotification])
         [self setTemporaryToolMode:SKNoToolMode];
-    if (inKeyWindow == NO && interactionMode == SKPresentationMode) {
-        [[self class] cancelPreviousPerformRequestsWithTarget:self selector:@selector(showNavWindow) object:nil];
-        [[self class] cancelPreviousPerformRequestsWithTarget:self selector:@selector(doAutoHide) object:nil];
-        [[self class] cancelPreviousPerformRequestsWithTarget:self selector:@selector(doAutoHideCursor) object:nil];
-        pdfvFlags.cursorHidden = 0;
-        [self setCursorForMouse:nil];
+    if (inKeyWindow == NO) {
+        if (interactionMode == SKPresentationMode) {
+            [[self class] cancelPreviousPerformRequestsWithTarget:self selector:@selector(showNavWindow) object:nil];
+            [[self class] cancelPreviousPerformRequestsWithTarget:self selector:@selector(doAutoHide) object:nil];
+            [[self class] cancelPreviousPerformRequestsWithTarget:self selector:@selector(doAutoHideCursor) object:nil];
+            pdfvFlags.cursorHidden = 0;
+            [self setCursorForMouse:nil];
+        } else if (toolMode == SKMagnifyToolMode) {
+            [loupeController hide];
+        }
     }
 }
 
