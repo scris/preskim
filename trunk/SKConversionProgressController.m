d73764ac67ac2f3b6161119b45b731a793522830
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2023-11-12 15:14:04 +0000

    look for conversion command in /opt/hoembrew/bin, for ghostscript installed by homebrew

--- trunk/SKConversionProgressController.m
+++ trunk/SKConversionProgressController.m
@@ -203,7 +203,7 @@ CGPSConverterCallbacks SKPSConverterCallbacks = {
 static NSString *toolPathForCommand(NSString *defaultKey, NSArray *supportedTools) {
     NSString *commandPath = [[NSUserDefaults standardUserDefaults] stringForKey:defaultKey];
     NSString *commandName = [commandPath lastPathComponent];
-    NSArray *paths = @[@"/Library/TeX/texbin", @"/usr/texbin", @"/sw/bin", @"/opt/sw/bin", @"/opt/local/bin", @"/usr/local/bin"];
+    NSArray *paths = @[@"/Library/TeX/texbin", @"/usr/texbin", @"/sw/bin", @"/opt/sw/bin", @"/opt/local/bin", @"/usr/local/bin", @"/opt/homebrew/bin"];
     NSInteger i = 0, iMax = [paths count];
     NSFileManager *fm = [NSFileManager defaultManager];
     NSEnumerator *toolEnum = [supportedTools objectEnumerator];
