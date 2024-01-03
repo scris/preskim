7015a51c17512c31114a4a8d95201b392cb54381
Christiaan Hofman <1167268+hofman@users.noreply.github.com>
2024-01-03 10:40:30 +0000

    Don't use year range in copyright notice in source files. Increase date range in info and Licence.

--- SkimNotes/SKNExtendedAttributeManager.m
+++ SkimNotes/SKNExtendedAttributeManager.m
@@ -4,7 +4,7 @@
 //
 //  Created by Adam R. Maxwell on 05/12/05.
 /*
- This software is Copyright (c) 2005-2023
+ This software is Copyright (c) 2005
  Adam R. Maxwell. All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
